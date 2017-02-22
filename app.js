7d9f660920fa0fb0fda9f71e984338b81e124374
Amit K Biswas <biswas1992@hotmail.com>
2017-02-22 11:41:57 -0500

    create name username and pasword by using argvs and declered callback function

--- app.js
+++ app.js
@@ -1,47 +1,71 @@
-console.log('Starting Password Manager App ');
+console.log('starting password manager');
 
 var storage = require('node-persist');
-storage.initSync(); // create * name , username, password
+storage.initSync();
 
-// // storage.setItemSync('accounts', [{
-// //     username: 'Amit Biswas',
-// //     balance: 0
-// // }]);
+var argv = require('yargs')
+    .command('create', 'Create a new account', function(yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Account name (eg: Twitter, Facebook)',
+                type: 'string'
+            },
+            username: {
+                demand: true,
+                alias: 'u',
+                description: 'Account username or email',
+                type: 'string'
+            },
+            password: {
+                demand: true,
+                alias: 'p',
+                description: 'Account password',
+                type: 'string'
+            }
+        }).help('help');
+    })
+    .command('get', 'Get an existing account', function(yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: 'Account name (eg: Twitter, Facebook)',
+                type: 'string'
+            }
+        }).help('help');
+    })
+    .help('help')
+    .argv;
+var command = argv._[0];
 
-// var accounts = storage.getItemSync('accounts');
+// create
+//     --name
+//     --username
+//     --password
 
-// //push on a new account
-// //save using setItemSync
-// accounts.push({
-//     username: 'Jen',
-//     balance: 75
-// });
+// get
+//     --name
 
-// storage.setItemSync('accounts', accounts);
-// console.log(accounts);
-//****************************/
-// account.name Facebook 
-//account.username  User 123!
-//account.password Password123!
+// account.name Facebook
+// account.username User12!
+// account.password Password123!
 
 function createAccount(account) {
     var accounts = storage.getItemSync('accounts');
-    //if accounts is undefined (use typeof)
-    //set accounts=[]
-    //push on new accounts and return account 
+
     if (typeof accounts === 'undefined') {
         accounts = [];
     }
+
     accounts.push(account);
     storage.setItemSync('accounts', accounts);
+
     return account;
 }
 
 function getAccount(accountName) {
-    //create a variable call account
-    //use getItemSync for load item whatever account is exist
-    //use loop for iterare over array reaturn matching account
-    //else undefined 
     var accounts = storage.getItemSync('accounts');
     var matchedAccount;
 
@@ -49,19 +73,26 @@ function getAccount(accountName) {
         if (account.name === accountName) {
             matchedAccount = account;
         }
-
-
     });
 
     return matchedAccount;
 }
 
-// createAccount({
-//     name: 'Snap',
-//     username: 'jem@snap.com',
-//     password: 'Password12'
+if (command === 'create') {
+    var createdAccount = createAccount({
+        name: argv.name,
+        username: argv.username,
+        password: argv.password
+    });
+    console.log('Account created!');
+    console.log(createdAccount);
+} else if (command === 'get') {
+    var fetchedAccount = getAccount(argv.name);
 
-// });
-
-var snapAccount = getAccount('Snap');
-console.log(snapAccount);
+    if (typeof fetchedAccount === 'undefined') {
+        console.log('Account not found');
+    } else {
+        console.log('Account found!');
+        console.log(fetchedAccount);
+    }
+}
